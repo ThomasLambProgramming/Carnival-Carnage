2c4234c9b6077e8d4c2e8f84a840324ac4a2df65
Mara <s200494@students.aie.edu.au>
2021-05-28 16:38:59 +1000

    Added hand animations and controller toggle

--- VR_Project/Assets/Scripts/HandPresence.cs
+++ VR_Project/Assets/Scripts/HandPresence.cs
@@ -13,9 +13,15 @@ public class HandPresence : MonoBehaviour
     private InputDevice targetDevice;
     private GameObject spawnedController;
     private GameObject spawnedHandModel;
+    private Animator handAnimator;
 
     // Start is called before the first frame update
     void Start()
+    {
+        TryInitialise();
+    }
+
+    void TryInitialise()
     {
         List<InputDevice> devices = new List<InputDevice>();
         InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
@@ -36,20 +42,60 @@ public class HandPresence : MonoBehaviour
         }
 
         spawnedHandModel = Instantiate(handModelPrefab, transform);
+        handAnimator = spawnedHandModel.GetComponent<Animator>();
+    }
+
+    void UpdateHandAnimation()
+    {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Trigger", 0);
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Grip", 0);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (showController)
+        if (!targetDevice.isValid)
         {
-            spawnedHandModel.SetActive(false);
-            spawnedController.SetActive(true);
+            TryInitialise();
         }
         else
         {
-            spawnedHandModel.SetActive(true);
-            spawnedController.SetActive(false);
+            if (showController)
+            {
+                spawnedHandModel.SetActive(false);
+                spawnedController.SetActive(true);
+            }
+            else
+            {
+                spawnedHandModel.SetActive(true);
+                spawnedController.SetActive(false);
+                UpdateHandAnimation();
+            }
+        }
+
+        targetDevice.TryGetFeatureValue(CommonUsages.primaryTouch, out bool primaryTouchValue);
+        if (primaryTouchValue)
+        {
+            showController = true;
+        }
+        else
+        {
+            showController = false;
         }
     }
 }
