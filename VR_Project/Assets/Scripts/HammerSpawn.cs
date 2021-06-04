e4f140b500b23f6515a70ef76dd926a606b3109f
Mara <s200494@students.aie.edu.au>
2021-06-04 14:01:13 +1000

    Added object deletion, updated hammer spawn script

--- VR_Project/Assets/Scripts/HammerSpawn.cs
+++ VR_Project/Assets/Scripts/HammerSpawn.cs
@@ -3,50 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem.XR;
 using UnityEngine.XR;
+using UnityEngine.UI;
+using System.Linq;
 
 public class HammerSpawn : MonoBehaviour
 {
-    private bool alreadySpawned = false;
     public GameObject HammerPrefab = null;
-    private GameObject CurrentHammer = null;
+    private bool spawnHammer = false;
 
-    public InputDeviceCharacteristics controllerCharacteristics;
-    private InputDevice controller;
+    private GameObject CurrentHammer = null;
+    private InputDevice device;
 
     public void Start()
     {
+        XRNode xrNodeRight = XRNode.RightHand;
         List<InputDevice> devices = new List<InputDevice>();
-        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
-
-        if (devices.Count > 0)
-        {
-            controller = devices[0];
-        }
-
-        SpawnHammer();
+        InputDevices.GetDevicesAtXRNode(xrNodeRight, devices);
+        device = devices.FirstOrDefault();
     }
 
     public void SpawnHammer()
     {
         CurrentHammer = Instantiate(HammerPrefab, transform);
         CurrentHammer.transform.localRotation.Set(0, 90, 0, 0);
-        CurrentHammer.GetComponent<Rigidbody>().isKinematic = true;
-
-        alreadySpawned = true;
     }
 
-    public void OnTriggerEnter(Collider collider)
+    public void Update()
     {
-        if (collider.gameObject.GetComponentInChildren<XRController>().added)
+        if (spawnHammer && CurrentHammer == null)
         {
-            controller.TryGetFeatureValue(CommonUsages.triggerButton, out bool triggerButton);
-            controller.TryGetFeatureValue(CommonUsages.gripButton, out bool gripButton);
+            device.TryGetFeatureValue(CommonUsages.triggerButton, out bool triggerButton);
+            device.TryGetFeatureValue(CommonUsages.gripButton, out bool gripButton);
 
             if (triggerButton && gripButton)
             {
-                CurrentHammer.GetComponent<Rigidbody>().isKinematic = false;
                 SpawnHammer();
             }
+        }
+        
+        if (CurrentHammer != null)
+        {
+            if (!CurrentHammer.activeInHierarchy)
+            {
+                CurrentHammer = null;
+            }
+        }
+    }
+
+    public void OnTriggerStay(Collider collider)
+    {
+        if (collider.name == "LeftHand Collider" || collider.name == "RightHand Collider")
+        {
+            spawnHammer = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider collider)
+    {
+        if (collider.name == "LeftHand Collider" || collider.name == "RightHand Collider")
+        {
+            spawnHammer = false;
         }
     }
 }
