fc149f4ac15d3d24bd0098314d0998adbf7ff72f
Oscar <oscarbuzzsmith@gmail.com>
2021-06-10 16:50:43 +1000

    Working on UI

---
+++ VR_Project/Assets/Scenes/Oscar/Scripts/SimpleCamera.cs
@@ -0,0 +1,26 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class SimpleCamera : MonoBehaviour
+//{
+//    [SerializeField]
+//    [Range(1, 1000)] float mouseSensitivity = 200;
+    
+//    private float xRotation = 0f;
+//    private float yRotation = 0f;
+
+//    void Update()
+//    {
+//        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+//        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+//        xRotation -= mouseY;
+//        yRotation -= mouseX;
+//        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+//        yRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+//        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+
+//    }
+//}
---
+++ VR_Project/Assets/Scenes/Oscar/Scripts/VRUIButtonBrain.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.SceneManagement;
+
+public class VRUIButtonBrain : MonoBehaviour
+{
+    #region MATERIALS
+
+    // materials
+    [SerializeField]
+    private Material DefaultMaterial;   // default material for the button when it is not at all interacted with 
+
+    [SerializeField]
+    private Material HighlightMaterial; // material for when the button is pointed at, representing which button is going to be selected
+
+    [SerializeField]
+    private Material PressMaterial;     // material for when the button is pressed 
+
+    #endregion
+
+    #region INPUT
+
+    // input
+    private InputDevice LeftController;
+    private InputDevice RightController;
+
+    #endregion
+
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics leftControllerCharacteristics = InputDeviceCharacteristics.Left;
+        InputDevices.GetDevicesWithCharacteristics(leftControllerCharacteristics, devices);
+        if (devices.Count > 0)
+        {
+            LeftController = devices[0];
+        }
+        devices.Clear();
+        devices = new List<InputDevice>();
+        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right;
+        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+        if (devices.Count > 0)
+        {
+            RightController = devices[0];
+        }
+    }
+
+    void Update()
+    {
+        // leftJoyStick.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out bool resetScene);
+
+        //LeftController.TryGetFeatureUsages(CommonUsages.)
+        //if (Physics.Raycast()
+    }
+}
