7332603e5a67b1cc8f10f2fa709eeb18c075ae62
Oscar <oscarbuzzsmith@gmail.com>
2021-06-11 15:18:35 +1000

    Updating Scoreboard

--- VR_Project/Assets/Scripts/Score_Board.cs
+++ VR_Project/Assets/Scripts/Score_Board.cs
@@ -1,21 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Score_Board : MonoBehaviour
 {
-    [SerializeField]
-
-
-
+    private TextMeshPro TimerText;
+    private TextMeshPro ScoreText;
 
     void Start()
     {
-       
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(i).name == "Timer")
+            {
+                try { TimerText = transform.GetChild(i).GetComponent<TextMeshPro>(); }
+                catch { }
+            }
+        }
     }
 
     void Update()
     {
-        
+        TimerText.text = Timer.time;
     }
 }
