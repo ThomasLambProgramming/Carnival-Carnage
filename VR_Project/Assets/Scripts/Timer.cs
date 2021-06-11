c80a02eca74f18d76f5936f64d84ec51f176bf4b
Mara <s200494@students.aie.edu.au>
2021-06-11 10:20:56 +1000

    Added timer

---
+++ VR_Project/Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public static string time;
+    public bool isFinished = false;
+    public float timeRemaining = 120;
+
+    public Text debug;
+
+    private void Update()
+    {
+        if (timeRemaining > 0 && !isFinished)
+        {
+            timeRemaining -= Time.deltaTime;
+            SetTimer(timeRemaining);
+        }
+    }
+
+    private void SetTimer(float a_time)
+    {
+        float minutes = (int)a_time / 60;
+        float seconds = a_time % 60;
+
+        time = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        debug.text = time;
+    }
+}
