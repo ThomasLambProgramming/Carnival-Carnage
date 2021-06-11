b9d049cf36f065aa49713b3af2b7ea85ad29c9dd
Oscar <oscarbuzzsmith@gmail.com>
2021-06-11 11:06:56 +1000

    Adding UI Prefab

---
+++ VR_Project/Assets/Scripts/UI_MainMenu_Brain.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_MainMenu_Brain : MonoBehaviour
+{
+    public void OnMainMenuButtonPress(int Level)
+    {
+        try { SceneManager.LoadScene(Level, LoadSceneMode.Single); }
+        catch { Debug.Log("ERROR : That scene does not exist!"); }
+    } // loads a scene based on the level number or scene index int he build settings. If errors occur, check the build settings and double check the index of the desired level   
+}
